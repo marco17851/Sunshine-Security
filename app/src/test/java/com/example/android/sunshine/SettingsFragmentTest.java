ece23b9b87cb37e625e7871aff4ff8942f8534f2
Marco Barragan <mbarraga@thoughtworks.com>
2017-07-17 17:13:52 -0400

    Add tests for Settings Fragment

--- app/src/test/java/com/example/android/sunshine/SettingsFragmentTest.java
+++ app/src/test/java/com/example/android/sunshine/SettingsFragmentTest.java
@@ -1,9 +1,21 @@
 package com.example.android.sunshine;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.preference.PreferenceManager;
+import android.view.View;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
+import org.robolectric.RuntimeEnvironment;
 import org.robolectric.annotation.Config;
+import org.robolectric.shadows.support.v4.SupportFragmentTestUtil;
 
 import static org.junit.Assert.*;
 
@@ -11,9 +23,38 @@ import static org.junit.Assert.*;
 @Config(constants = BuildConfig.class)
 public class SettingsFragmentTest {
 
+    SettingsFragment fragment;
+    Context applicationContext;
+    SharedPreferences sharedPreferences;
+
+    @Before
+    public void setUp(){
+        fragment = new SettingsFragment();
+
+        applicationContext = RuntimeEnvironment.application.getApplicationContext();
+
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(applicationContext);
+    }
+
+    @Test
+    public void preferencesFragmentShouldShowDefaultNotificationsValue() {
+        SupportFragmentTestUtil.startFragment(fragment, SettingsActivity.class);
+
+        assertEquals(true, sharedPreferences.getBoolean("enable_notifications", false));
+    }
+
+    @Test
+    public void userShouldBeAbleToPreventNotifications() {
+        sharedPreferences.edit().putBoolean("enable_notifications", false).apply();
+        SupportFragmentTestUtil.startFragment(fragment, SettingsActivity.class);
+
+        assertEquals(false, sharedPreferences.getBoolean("enable_notifications", true));
+    }
+
     @Test
     public void shouldLetUserAddMultipleLocations() {
         assertEquals(1+1, 2);
+
     }
 
     @Test
